309e05b6bed19fd9cb1ff0cbbc19c670f7de3b3a
CarranzaXav <carranzax7@gmail.com>
2024-12-14 11:25:53 -0500

    index.js error fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { store } from './app/store'
 import {Provider} from 'react-redux'
 import {disableReactDevTools} from '@fvilers/disable-react-devtools'
 
-if(process.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   disableReactDevTools()
 }
 
