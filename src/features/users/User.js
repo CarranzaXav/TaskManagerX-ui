6f75ec4749207915434454022010af647e468a8d
CarranzaXav <carranzax7@gmail.com>
2025-06-01 17:40:21 -0400

    User.jsx

--- src/features/users/User.js
+++ src/features/users/User.js
@@ -18,7 +18,7 @@ const User = ({ userId }) => {
   if (user) {
     const handleEdit = () => navigate(`/dash/users/${userId}`);
 
-    const userRolesString = user.roles.toString().replaceAll(",", ",");
+    const userRolesString = user.roles.toString().replaceAll(",", ", ");
 
     const cellStatus = user.active ? "" : "users__cell--inactive";
 
